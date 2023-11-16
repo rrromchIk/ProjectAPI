1839315a98caaa25efb66c92c391110155528bc1
Roma <nikitinroma2605@gmail.com>
2023-11-16 19:16:21 +0200

    added employee list to the team response

--- Dto/TeamResponse.cs
+++ Dto/TeamResponse.cs
@@ -1,8 +1,11 @@
-﻿namespace ProjectAPI.Dto; 
+﻿using ProjectAPI.Models;
+
+namespace ProjectAPI.Dto; 
 
 public class TeamResponse {
     public int Id { get; set; }
     
     public string Name { get; set; } = null!;
-    public ICollection<ProjectDto> Projects { get; set; }
+    public ICollection<ProjectDto> Projects { get; set; } = null!;
+    public ICollection<EmployeeDto> Employees { get; set; } = null!;
 }
